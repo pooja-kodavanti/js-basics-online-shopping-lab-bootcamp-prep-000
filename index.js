0d62e9e749ef5d8529cba16ebb3e99444ceb0e51
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-23 01:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var price = Math.floor((Math.random() * 100) + 1)
- cart.push({item: price})
+ cart[item] = price
+
+ return cart
 
  }
 
