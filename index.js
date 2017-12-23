5fbb5a1f6ff579db4f99481c2964e166e7396673
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-23 02:39:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  let itemsWithPrices = []
   for (i = 0, var l = cart.length; i < l; i++) {
     if (l === 0) {
       return "Your shopping cart is empty."
